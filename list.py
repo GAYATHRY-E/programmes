ea83026a0944141d75486b5fa54c9a1d013488b2
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-08 19:51:52 +0530

    list in python

---
+++ list.py
@@ -0,0 +1,27 @@
+          #creating a list
+nums=[1,2,4,5,6]
+nums
+          #creating another list
+names=['ammu','akku','raju','ravi']
+names
+          #list of list
+LIST=[nums,names]
+LIST
+          #insert value to list ti index 2
+nums.insert(2,96)
+nums
+           #append a value to list
+nums.append(36)
+nums
+           #remove last element
+nums.pop()
+nums
+           #removing an element from a particular position(index)
+nums.pop(2)
+nums
+           #removing multiple values
+del nums[2:]
+nums
+           #to add set of values
+nums.extend([7,8,9,10])
+nums
