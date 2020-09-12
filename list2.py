9a024f0be11a4737dbed0569a1f995123907cb94
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-12 13:13:48 +0530

    count, copy, reverse, clear in a list

---
+++ list2.py
@@ -0,0 +1,15 @@
+>>> list=[1,2,5,1,6,2,1]
+  #count of a number in a list
+>>> list.count(1)
+3
+  #copying a list
+>>> list.copy()
+[1, 2, 5, 1, 6, 2, 1]
+  #reversing a list
+>>> list.reverse()
+>>> list
+[1, 2, 6, 1, 5, 2, 1]
+  #clearimg a list
+>>> list.clear()
+>>> list
+[]
