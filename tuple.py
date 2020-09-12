9999ad763b6bc7babcfe783434d28f8f5afcd2be
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-12 13:05:13 +0530

    tuple in python

---
+++ tuple.py
@@ -0,0 +1,10 @@
+     #creating a tuple
+>>>tup=(1,2,1,3,1,6)
+>>>tup
+    #count of a number in tuple
+>>> tup.count(1)
+3
+    #index of a value in tuple
+>>> tup.index(6)
+5
+#tuple is imutable..becz we can't change the values
