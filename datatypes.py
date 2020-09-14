9d8eb62d5b04c546e18df83d19e69bf47d9fd1cf
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-14 20:56:10 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+#finding datatype
+#datatypes in python(8)
+#None,Numeric,List, Tuple,Set, String,Range, Dictionary
+>>> type(a)
+<class 'int'>
+>>> int(5.66489)
+5
+>>> float(569)
+569.0
+>>> complex(5,6)
+(5+6j)
+>>> range(0,10)
+range(0, 10)
+>>> list(range(0,10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> list(range(0,10,2))
+[0, 2, 4, 6, 8]
+>>> list(range(0,10,3))
+[0, 3, 6, 9]
+>>> a=4
+>>> b=6
+>>> a<b
+True
+>>> b<a
+False
+>>> a=b
+>>> 
