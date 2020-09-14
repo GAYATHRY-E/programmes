7dfcb1b7dbbfa07ca8b20059343e1d834a300839
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-14 21:07:48 +0530

    numbersystem

---
+++ Numbersystem.py
@@ -0,0 +1,18 @@
+#converting decimal into binary
+>>> bin(25)
+'0b11001'
+#converting decimal into hexadecimal
+>>> hex(25)
+'0x19'
+#coverting decimal into octadecimal
+>>> oct(25)
+'0o31'
+#converting binary into decimal
+>>> 0b0101
+5
+#converting octal into decimal
+>>> 0o31
+25
+#coverting hexadecimal into decimal
+>>> 0x19
+25
