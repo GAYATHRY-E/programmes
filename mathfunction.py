4d51f69aaf5cd1d432fdb7770783cf7cc074c704
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-16 15:50:25 +0530

    math functions in python

---
+++ mathfunction.py
@@ -0,0 +1,30 @@
+#importing math module
+>>> import math
+>>> print(math.sqrt(25))
+5.0
+>>> import math as m
+>>> x=m.sqrt(36)
+>>> x
+6.0
+>>> x=int(m.sqrt(36))
+>>> x
+6
+>>> 
+>>> print(m.floor(2.6))
+2
+>>> print(m.ceil(2.6))
+3
+>>> print(m.pow(2,3))
+8.0
+>>> print(int(m.pow(2,3)))
+8
+>>> print(m.pi)
+3.141592653589793
+>>> print(m.e)
+2.718281828459045
+>>> from math import sqrt,pow
+>>> sqrt(36)
+6.0
+>>> pow(2,3)
+8.0
+>>> 
