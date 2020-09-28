b78075f5804125454a5bd8e1f2a8f3ef06d1a64d
Gayathry.E <57176258+GAYATHRY-E@users.noreply.github.com>
2020-09-28 09:05:54 +0530

    continue, break , pass

---
+++ continuebreakpass.py
@@ -0,0 +1,17 @@
+#continue
+for i in range(1,20):
+  if(i%3==0):
+    continue
+  print(i)
+  
+#break
+for i in range(1,20):
+  if(i%3==0):
+    break
+  print(i)
+  
+#pass
+for i in range(1,20):
+  if(i%3==0):
+    pass
+  print(i)
